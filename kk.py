49e145469b02e801811e8d229943a9900cae142c
LukMRVC <sydnexpklover@gmail.com>
2017-12-01 10:24:36 +0100

    vylistovani

--- kk.py
+++ kk.py
@@ -4,12 +4,16 @@ api = API(host='http://192.168.22.188:81', username='Dave', password='dbvjdu123'
 
 
 
-with open('/home/ucitel/Plocha/bcrypt.c') as file_object:
+'''with open('/home/ucitel/Plocha/bcrypt.c') as file_object:
     response = api.documents.documents.post({'document_type': 2}, files={'file': file_object})
-
+'''
 #print (response)
 
 
 #print(api.document_indexing.indexes.get())
-#print(api.metadata.metadata_types.get())
-print(api.documents.documents_type.get())
+for result in api.metadata.metadata_types.get()['results']:
+    print(result['name'])
+
+for result in api.documents.document_types.get()['results']:
+    print(result['label'])
+
